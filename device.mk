981783c0af8978f3c35c3f0d061098013785d2c8
Shirayuki39 <lorddemecrius83@proton.me>
2024-09-04 21:21:12 +0800

    X678B: overlay: Build SettingsResOverlay
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -223,6 +223,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworkResOverlayVeritas \
+    SettingsResOverlayVeritas \
     SystemUIResOverlayVeritas \
     TetheringResOverlayVeritas \
     WifiResOverlayVeritas
