403be2bff76885204ff93f76262207e7cd428594
Shirayuki39 <lorddemecrius83@proton.me>
2025-07-29 13:32:46 +0900

    X678B: Prepare template system and vendor properties
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,9 @@
 
 DEVICE_PATH := device/infinix/X678B
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Inherit the proprietary files
 include vendor/infinix/X678B/BoardConfigVendor.mk
