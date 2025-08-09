b63ef70a142c72bf4b2550f5831564b7c3573d51
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-09 23:13:15 +0900

    X678B: AxionAOSP Bringup
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- lineage_X678B.mk
+++ lineage_X678B.mk
@@ -26,5 +26,18 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X678B \
     BuildFingerprint=Infinix/X678B-OP/Infinix-X678B:14/UP1A.231005.007/250217V838:user/release-keys
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+# AxionAOSP Flags
+AXION_CAMERA_REAR_INFO := 108
+AXION_CAMERA_FRONT_INFO := 32
+AXION_MAINTAINER := Shirayuki39
+AXION_PROCESSOR := Helio_G99
+PRODUCT_NO_CAMERA := false
+BYPASS_CHARGE_SUPPORTED := false
+TARGET_USES_USLMK := false
+
+# Enable activity open override fix for low-end devices or devices affected by activity open/exit freezing issue 
+PERF_ANIM_OVERRIDE := true
+
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
