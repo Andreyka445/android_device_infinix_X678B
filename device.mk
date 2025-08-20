36c86eef8fbc1c0325e068070ddb2ab32fe8599a
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-20 09:16:02 +0900

    X678B: Setup CustomOTA
    * Allows unofficial builds to recieve OTA by redirecting OTA fetch location to our custom ota repo
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayVeritas \
     SystemUIResOverlayVeritas \
     TetheringResOverlayVeritas \
+    UpdaterOverlayVeritas \
     WifiResOverlayVeritas
 
 # Lineage-Specific Overlays
